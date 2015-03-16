2219dffef1700a004d8f1b0cc7e26569bd102e5d
Samuel Martin <s.martin49@gmail.com>
2015-03-16 16:38:47 +0100

    toolchain/helpers: add openmp check
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>

--- toolchain/helpers.mk
+++ toolchain/helpers.mk
@@ -326,6 +326,20 @@ check_fortran = \
 	fi
 
 #
+# Check that the external toolchain supports OpenMP
+#
+# $1: cross-gcc path
+#
+check_openmp = \
+	__CROSS_CC=$(strip $1) ; \
+	printf '\#include <omp.h>\nint main(void) { return omp_get_num_procs(); }\n' | \
+		$${__CROSS_CC} -fopenmp -lgomp -x c -o /dev/null - ; \
+	if test $$? -ne 0 ; then \
+		echo "OpenMP support is selected but is not available in external toolchain" ; \
+		exit 1 ; \
+	fi
+
+#
 # Check that the cross-compiler given in the configuration exists
 #
 # $1: cross-gcc path
