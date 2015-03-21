95c16b3d93ecb12ec6837d367915423d374e8244
Samuel Martin <s.martin49@gmail.com>
2015-03-21 11:40:07 +0100

    docs/manual: allow to override the python interpreter
    As stated in [1], the gen-manual-lists.py script cannot be run using
    python3; so use PYTHON2 variable to allow setting the python interpreter
    on the 'make manual' command line.
    
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>

--- docs/manual/manual.mk
+++ docs/manual/manual.mk
@@ -7,6 +7,8 @@
 MANUAL_SOURCES = $(sort $(wildcard docs/manual/*.txt) $(wildcard docs/images/*))
 MANUAL_RESOURCES = $(TOPDIR)/docs/images
 
+PYTHON2 ?= python
+
 # Our manual needs to generate lists
 # Packages included in BR2_EXTERNAL are not part of buildroot, so they
 # should not be included in the manual.
@@ -14,13 +16,13 @@ define MANUAL_GEN_LISTS
 	$(Q)$(call MESSAGE,"Updating the manual lists...")
 	$(Q)BR2_DEFCONFIG="" TOPDIR=$(TOPDIR) O=$(@D) \
 		BR2_EXTERNAL=$(TOPDIR)/support/dummy-external \
-		python -B $(TOPDIR)/support/scripts/gen-manual-lists.py
+		$(PYTHON2) -B $(TOPDIR)/support/scripts/gen-manual-lists.py
 endef
 MANUAL_POST_RSYNC_HOOKS += MANUAL_GEN_LISTS
 
 # Our list-generating script requires argparse
 define MANUAL_CHECK_LISTS_DEPS
-	$(Q)if ! python -c "import argparse" >/dev/null 2>&1 ; then \
+	$(Q)if ! $(PYTHON2) -c "import argparse" >/dev/null 2>&1 ; then \
 		echo "You need python with argparse on your host to generate" \
 			"the list of packages in the manual"; \
 		exit 1; \
